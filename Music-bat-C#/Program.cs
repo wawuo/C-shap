d1341e8d2b67c55e0afae9541f360c3717daf036
zhld1980@gmail.com <46234182@qq.com>
2023-04-19 16:18:55 +0800

    这段代码的作用是检查是否存在足够的 MP3 文件进行播放，如果存在，则先将最旧的三个文件移动到一个指定样本目录下，并使用 VLC 播放器播放这些文件。 ------------------------------- 在处理原始代码时，我注意到当将文件移动到指定目录时，代码未将新路径添加到变量 arg 中。因此，在修正后的代码中，我将该变量更新为包含新路径的完整字符串，并将其传递给 VLC 播放器。 此外，我们注释掉了之前循环处理所有样本目录下的 MP3 文件的代码段，这是因为我们只需要播放已移动到指定目录内的三个 MP3 文件。
    最后，我们等待指定播放时间之后退出程序，以确保 VLC 播放器有足够的时间来完成播放任务。
    请注意，以上更新部分仅限于 MP3 文件的情况，请根据需求和可支持的文件类型相应调整。

--- Music-bat-C#/Program.cs
+++ Music-bat-C#/Program.cs
@@ -12,8 +12,8 @@ namespace PlayMp3Files
             string sampleDir = @"F:\mp3样本";
             string playerPath = @"C:\Program Files\VideoLAN\VLC\vlc.exe";
             string neCloudMusicPath = @"E:\Program Files\Netease\CloudMusic\cloudmusic.exe";
-
             string configPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "config.ini");
+			
 
             if (File.Exists(configPath))
             {
@@ -41,6 +41,12 @@ namespace PlayMp3Files
             //获取目录下所有的 MP3 文件并按修改时间排序
             DirectoryInfo di = new DirectoryInfo(musicDir);
             FileInfo[] files = di.GetFiles("*.mp3");
+			int count = files.Length;
+
+			// 输出 mp3 文件个数
+			Console.WriteLine("该文件夹中的 mp3 文件数量为：" + count);	
+		
+			
             Array.Sort<FileInfo>(files, (a, b) => a.LastWriteTime.CompareTo(b.LastWriteTime)); // 按照最旧的文件优先排序
 
             if (!Directory.Exists(sampleDir))
@@ -61,22 +67,23 @@ namespace PlayMp3Files
                 string arg = @"--no-qt-privacy-ask --playlist-tree";
 
                 //如果文件数量大于等于3，则将最旧的三个 MP3 移至指定样本目录
-                for (int i = 0; i < 3 && i < files.Length; i++)
-                {
-                    string newPath = Path.Combine(sampleDir, files[i].Name);
+			for (int i = 0; i < 3 && i < files.Length; i++)
+					{
+						string newPath = Path.Combine(sampleDir, files[i].Name);
 
-                    if (!File.Exists(newPath))
-                    {
-                        //剪切文件到指定目录
-                        File.Move(files[i].FullName, newPath);
-                    }
-                }
+						if (!File.Exists(newPath))
+						{
+							//剪切文件到指定目录
+							File.Move(files[i].FullName, newPath,true);
+						
+							// 更新要播放的文件参数字符串
+							arg += " \"" + newPath + "\"";
+							Console.WriteLine("要播放的文件" + newPath);
+						}
+					}
 
                 //使用 VLC 播放样本目录下的三个 MP3
-                foreach (FileInfo file in new DirectoryInfo(sampleDir).GetFiles("*.mp3"))
-                {
-                    arg += " \"" + file.FullName + "\"";
-                }
+              
                 Console.WriteLine("启动 VLC 播放器...");
                 Process.Start(playerPath, arg);
 
@@ -86,18 +93,12 @@ namespace PlayMp3Files
         }
     }
 }
-/* ```
+/*-------------------------
+这段代码的作用是检查是否存在足够的 MP3 文件进行播放，如果存在，则先将最旧的三个文件移动到一个指定样本目录下，并使用 VLC 播放器播放这些文件。
+-------------------------------
+在处理原始代码时，我注意到当将文件移动到指定目录时，代码未将新路径添加到变量 arg 中。因此，在修正后的代码中，我将该变量更新为包含新路径的完整字符串，并将其传递给 VLC 播放器。
+此外，我们注释掉了之前循环处理所有样本目录下的 MP3 文件的代码段，这是因为我们只需要播放已移动到指定目录内的三个 MP3 文件。
 
-
-更改了几个地方：
-
-1. 注释中提到的小错误已经修正。
-
-2. 在复制文件操作的基础上，使用 `File.Move()` 方法将最新的 3 个 MP3 文件“剪切”到指定样本目录，而不是复制到该目录。
-
-3. 最后，在播放完毕后等待最后一个文件播放结束，并关闭程序。文件在 `sampleDir` 目录中保持不变，不删除任何文件。
-
-希望这个修改能够解决您的问题。如有任何问题，请随时告诉我。 
--------------------------
-在此版本中，我修改了 `Array.Sort()` 的排序方式，将最旧的文件排在前面。同时，在复制文件操作的基础上，使用 `File.Move()` 方法将最旧的3个 MP3 文件“剪切”到指定样本目录，而不是复制到该目录。然后使用 VLC 播放器播放这三个文件，并等待最后一个文件播放结束后退出程序。
-/* ```
+最后，我们等待指定播放时间之后退出程序，以确保 VLC 播放器有足够的时间来完成播放任务。
+请注意，以上更新部分仅限于 MP3 文件的情况，请根据需求和可支持的文件类型相应调整。
+*/
